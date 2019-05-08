0d277a76f3f3fd0afb103693a60a5c6692f23cb6
Larry Presswood <larry.presswood@regions.com>
2019-05-08 14:03:48 -0400

    start working on base amiPipeline DSL

---
+++ src/amiPipeline.groovy
@@ -0,0 +1,24 @@
+
+// The call(body) method in any file in workflowLibs.git/vars is exposed as a
+// method with the same name as the file.
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+
+    stages {
+        stage('validate') {
+            steps {
+                sh "${config.packer} validate jenkins-packer.json"
+            }
+        }
+        stage('build') {
+            steps {
+                withCredentials([[$class: 'AmazonWebServicesCredentialsBinding', credentialsId: 'AWS_CREDENTIALS']]) {
+                    sh "${config.packer} build ${config.packerFile}"
+                }
+            }
+        }
+    }
+}
